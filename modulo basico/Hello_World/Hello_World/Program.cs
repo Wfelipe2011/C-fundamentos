04c7ef39f34accb2b9c9afbf0f568bf85983c60c
Wilson Felipe <wfelipe2011@gmail.com>
2021-01-18 21:54:35 -0300

    Modulo basico

---
+++ modulo basico/Hello_World/Hello_World/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hello_World
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Indentação automatica CRTL + k + D
+            // Rodar o projeto CRTL + F5
+
+
+
+        }
+    }
+}
