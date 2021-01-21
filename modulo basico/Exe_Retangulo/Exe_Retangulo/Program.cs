c39e21c8fa9289fa744bafdced108536b29adc02
Wilson Felipe <wfelipe2011@gmail.com>
2021-01-21 13:10:29 -0300

    Exe.. Retangulo

---
+++ modulo basico/Exe_Retangulo/Exe_Retangulo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exe_Retangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo r = new Retangulo();
+
+            Console.Write("Entre com a Largura: ");
+            r.Largura = int.Parse(Console.ReadLine());
+            Console.Write("Entre com a Altura: " );
+            r.Altura = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Area: " + r.Area());
+            Console.WriteLine("Perimetro: ");
+            Console.WriteLine("Diagonal: ");
+        }
+    }
+}
---
+++ modulo basico/Exe_Retangulo/Exe_Retangulo/Retangulo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exe_Retangulo
+{
+    class Retangulo
+    {
+        public int Largura;
+        public int Altura;
+
+
+        public int Area()
+        {
+            return Largura * Altura;
+        }
+    }
+}
