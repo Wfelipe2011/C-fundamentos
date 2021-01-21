8a13b10e4be19c4562b0426b1e1e7bae29e0570a
Wilson Felipe <wfelipe2011@gmail.com>
2021-01-20 21:57:44 -0300

    Mais exercicios

---
+++ modulo basico/Sintaxe_Funcoes/Sintaxe_Funcoes/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Sintaxe_Funcoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Word: ");
+            Console.WriteLine(" Math.Sqrt()- Raiz ");
+            // Debug Break point, marcaçao F9
+            // Debug Para iniciar F5 e F10 para ir proximo passo.
+            // Debug O F11 ele entra dentro da funçao
+            // As funçoes sao chamadas de metodos.
+            // Reaproveitamento
+            Console.WriteLine("--------------- ");
+            Console.WriteLine("Digite 3 numeros: ");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            double resultado = Maior(n1, n2, n3);
+
+            Console.WriteLine("O maior é " + resultado);
+
+        }
+
+        static int Maior(int a, int b, int c)
+        {// int ´t o tipo da funçao, Maior é nome. a, b, c sao os parametros
+            int m;
+            if (a > b && a > c)
+            {
+                m = a;
+            }
+            else if (b > c)
+            {
+                m = b;
+            }
+            else
+            {
+                m = c;
+            }
+            return m;
+        }
+
+
+
+
+
+    }
+}
---
+++ modulo intermediario O.O/area_Triangulo_O_O/area_Triangulo_O_O/Triangulo.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace area_Triangulo_O_O
+{
+    class Triangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+
+       public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C)); // Ou
+            // double raiz = Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+            // return raiz; 
+
+        }
+    }
+}
