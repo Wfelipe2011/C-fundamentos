a3ed027fc205b6564ec94fa528cdea25e7d5d222
Wilson Felipe <wfelipe2011@gmail.com>
2021-01-22 20:38:27 -0300

    Media Nota

---
+++ modulo intermediario O.O/NotaAluno/NotaAluno/Media.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NotaAluno
+{
+    class Media
+    {
+        
+        public static double NotaMedia(double a , double b , double c)
+        {
+            double media = a + b + c;
+            return media;
+        }
+
+        public static string Situacao(double media)
+        {
+            string situacao;
+            if (media >= 60)
+            {
+                situacao = "APROVADO!";
+            }
+            else
+            {
+                situacao = "Reprovado!";
+            }
+            return situacao;
+        }
+    }
+}
---
+++ modulo intermediario O.O/NotaAluno/NotaAluno/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace NotaAluno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            double n1, n2, n3, media;
+            Console.Write("Nome do aluno: ");
+            string x = Console.ReadLine();
+            Console.WriteLine("Digite as três notas do aluno: ");
+            n1 = double.Parse(Console.ReadLine(), CI);
+            n2 = double.Parse(Console.ReadLine(), CI);
+            n3 = double.Parse(Console.ReadLine(), CI);
+
+            media = Media.NotaMedia(n1, n2, n3);
+
+            Console.WriteLine("Nota Final = " + media.ToString("F2"), CI);
+            Console.WriteLine(Media.Situacao(media));
+        }
+    }
+}
